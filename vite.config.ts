4bcb69330b2f7a4bb3c8e50ed8b4d8ebbf838c16
rae <josephonofiok08@gmail.com>
2026-02-25 00:44:59 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 export default defineConfig({
   root: "client", // your project root
   plugins: [react()],
-  base: "//", // optional, if you need it for CDN or relative paths
+  base: "/chery/", // optional, if you need it for CDN or relative paths
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "client/src"), // make sure this points to the correct src
